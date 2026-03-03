3d8d0dac4f8904cf327210162034d80340248ec7
Kimhan Jongjaidee <lnwzakimz@gmail.com>
2026-03-04 00:31:39 +0700

    fix the build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input || true
+python manage.py createsuperuser --no-input || echo "Superuser already exists"
